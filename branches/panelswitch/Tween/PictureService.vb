fea69ccfa885f850ea16aebad3183f9eb395225e
kiri_feather <kiri_feather@e39ad16e-3079-482e-bb30-4b4d378143b6>
2010-06-23 11:29:17 +0000

    TwitVideoに対応
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween@534 e39ad16e-3079-482e-bb30-4b4d378143b6

---
+++ branches/panelswitch/Tween/PictureService.vb
@@ -0,0 +1,58 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Xml
+
+Public Class PictureService
+    Private tw As Twitter
+
+    Public Function Upload(ByVal filePath As String, ByRef message As String, ByVal service As String) As String
+        Dim file As New FileInfo(filePath)
+        If Not file.Exists Then Return "Err:File isn't exists."
+        Dim st As Setting = Setting.Instance
+        Select Case service
+            Case "TwitPic"
+
+            Case "TwitVideo"
+                Return UpToTwitVideo(file, message)
+        End Select
+        Return ""
+    End Function
+
+    Private Function UpToTwitVideo(ByVal file As FileInfo, ByRef message As String) As String
+        Dim content As String = ""
+        Dim ret As HttpStatusCode
+        Dim svc As New TwitVideo
+        Try
+            ret = svc.Upload(file, message, "", tw.Username, tw.UserIdNo, content)
+        Catch ex As Exception
+            Return "Err:" + ex.Message
+        End Try
+        Dim url As String = ""
+        If ret = HttpStatusCode.OK Then
+            Dim xd As XmlDocument = New XmlDocument()
+            Try
+                xd.LoadXml(content)
+                Dim rslt As String = xd.SelectSingleNode("/rsp/@status").Value
+                If rslt = "ok" Then
+                    url = xd.SelectSingleNode("/rsp/mediaurl").InnerText
+                Else
+                    Return "Err:" + xd.SelectSingleNode("/rsp/err/@msg").Value
+                End If
+            Catch ex As XmlException
+                Return "Err:" + ex.Message
+            End Try
+        Else
+            Return "Err:" + ret.ToString
+        End If
+        If message.Length + url.Length + 1 > 140 Then
+            message = message.Substring(0, 140 - url.Length - 1) + " " + url
+        Else
+            message += " " + url
+        End If
+        Return tw.PostStatus(message, 0)
+    End Function
+
+    Public Sub New(ByVal twInstance As Twitter)
+        tw = twInstance
+    End Sub
+End Class
