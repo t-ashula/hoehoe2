64fd3dc73761a2dc941047f99008d62407159dad
syo68k <syo68k@e39ad16e-3079-482e-bb30-4b4d378143b6>
2010-08-21 15:57:38 +0000

    RaiseEventの頻度を抑えるようにした
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/branches/APIchangeevent@736 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/ApiInformation.vb
+++ Tween/ApiInformation.vb
@@ -1,50 +1,11 @@
 ﻿Public Class ApiInformationChangedEventArgs
     Inherits EventArgs
-
-    Public Property MaxCount As Integer
-        Get
-            Return ApiInformation._MaxCount
-        End Get
-        Set(ByVal value As Integer)
-            ApiInformation._MaxCount = value
-        End Set
-    End Property
-    Public Property RemainCount As Integer
-        Get
-            Return ApiInformation._RemainCount
-        End Get
-        Set(ByVal value As Integer)
-            ApiInformation._RemainCount = value
-        End Set
-    End Property
-    Public Property ResetTime As DateTime
-        Get
-            Return ApiInformation._ResetTime
-        End Get
-        Set(ByVal value As DateTime)
-            ApiInformation._ResetTime = value
-        End Set
-    End Property
-
-    Public Property ResetTimeInSeconds As Integer
-        Get
-            Return ApiInformation._ResetTimeInSeconds
-        End Get
-        Set(ByVal value As Integer)
-            ApiInformation._ResetTimeInSeconds = value
-        End Set
-    End Property
-
-    Public Property UsingCount As Integer
-        Get
-            Return ApiInformation._UsingCount
-        End Get
-        Set(ByVal value As Integer)
-            ApiInformation._UsingCount = value
-        End Set
-    End Property
+    Public MaxCount As Integer
+    Public RemainCount As Integer
+    Public ResetTime As New DateTime
+    Public ResetTimeInSeconds As Integer
+    Public UsingCount As Integer
 End Class
-
 
 Public Class ApiInfo
     Public MaxCount As Integer = -1
@@ -55,13 +16,13 @@ Public Class ApiInfo
 End Class
 
 Public Class ApiInformation
-    Friend Shared _MaxCount As Integer = -1
-    Friend Shared _RemainCount As Integer = -1
-    Friend Shared _ResetTime As New DateTime
-    Friend Shared _ResetTimeInSeconds As Integer = -1
-    Friend Shared _UsingCount As Integer = -1
+    Private Shared _MaxCount As Integer = -1
+    Private Shared _RemainCount As Integer = -1
+    Private Shared _ResetTime As New DateTime
+    Private Shared _ResetTimeInSeconds As Integer = -1
+    Private Shared _UsingCount As Integer = -1
 
-    Private Shared _lockobj As Object
+    Private Shared ReadOnly _lockobj As New Object
     Public Shared WithEvents ApiInformation As Object = New ApiInformation
 
     Public Shared Sub Initialize()
@@ -81,15 +42,25 @@ Public Class ApiInformation
     End Function
 
     Public Shared Event Changed(ByVal sender As Object, ByVal e As ApiInformationChangedEventArgs)
+    Public Shared Event RateLimitStatusHeaderChanged(ByVal sender As Object, ByVal e As ApiInformationChangedEventArgs)
 
     Private Shared Sub Raise_Changed()
         Dim arg As New ApiInformationChangedEventArgs
-        arg.MaxCount = MaxCount
-        arg.RemainCount = RemainCount
-        arg.ResetTime = ResetTime
-        arg.ResetTimeInSeconds = ResetTimeInSeconds
-        arg.UsingCount = UsingCount
+        SyncLock _lockobj
+            arg.MaxCount = MaxCount
+            arg.RemainCount = RemainCount
+            arg.ResetTime = ResetTime
+            arg.ResetTimeInSeconds = ResetTimeInSeconds
+            arg.UsingCount = UsingCount
+        End SyncLock
         RaiseEvent Changed(ApiInformation, arg)
+        SyncLock _lockobj
+            _MaxCount = arg.MaxCount
+            _RemainCount = arg.RemainCount
+            _ResetTime = arg.ResetTime
+            _ResetTimeInSeconds = arg.ResetTimeInSeconds
+            _UsingCount = arg.UsingCount
+        End SyncLock
     End Sub
 
     Public Shared Property MaxCount As Integer
@@ -97,8 +68,10 @@ Public Class ApiInformation
             Return _MaxCount
         End Get
         Set(ByVal value As Integer)
-            _MaxCount = value
-            Raise_Changed()
+            If _MaxCount <> value Then
+                _MaxCount = value
+                Raise_Changed()
+            End If
         End Set
     End Property
 
@@ -107,8 +80,10 @@ Public Class ApiInformation
             Return _RemainCount
         End Get
         Set(ByVal value As Integer)
-            _RemainCount = value
-            Raise_Changed()
+            If _RemainCount <> value Then
+                _RemainCount = value
+                Raise_Changed()
+            End If
         End Set
     End Property
 
@@ -117,8 +92,10 @@ Public Class ApiInformation
             Return _ResetTime
         End Get
         Set(ByVal value As DateTime)
-            _ResetTime = value
-            Raise_Changed()
+            If _ResetTime <> value Then
+                _ResetTime = value
+                Raise_Changed()
+            End If
         End Set
     End Property
 
@@ -127,8 +104,10 @@ Public Class ApiInformation
             Return _ResetTimeInSeconds
         End Get
         Set(ByVal value As Integer)
-            _ResetTimeInSeconds = value
-            Raise_Changed()
+            If _ResetTimeInSeconds <> value Then
+                _ResetTimeInSeconds = value
+                Raise_Changed()
+            End If
         End Set
     End Property
 
@@ -137,8 +116,10 @@ Public Class ApiInformation
             Return _UsingCount
         End Get
         Set(ByVal value As Integer)
-            _UsingCount = value
-            Raise_Changed()
+            If _UsingCount <> value Then
+                _UsingCount = value
+                Raise_Changed()
+            End If
         End Set
     End Property
 End Class
