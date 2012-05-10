2232f8a7f8a68f8fa6a4527a50798619fb602b62
t.ashula <office@ashula.info>
2012-05-10 22:22:44 +0900

    move interface to namespace

--- Tween.ConvertedToC#/Connection/IHttpConnection.cs
+++ Tween.ConvertedToC#/Connection/IHttpConnection.cs
@@ -54,6 +54,7 @@ namespace Tween
         /// <param name="code">APIメソッドの返したHTTPステータスコード</param>
         /// <param name="content">APIメソッドの処理結果</param>
         /// <remarks>contentはNothingになることがあるのでチェックを必ず行うこと</remarks>
-        public delegate void CallbackDelegate(object sender, ref HttpStatusCode code, ref string content);
     }
+
+    public delegate void CallbackDelegate(object sender, ref HttpStatusCode code, ref string content);
 }
