bd9d6b2791c04f43692177cdf710346057b8f421
t.ashula <office@ashula.info>
2012-05-14 03:33:05 +0900

    rename StatusDictionary.cs to TabClass.cs

--- Tween.ConvertedToC#/TabClass.cs
+++ Tween.ConvertedToC#/TabClass.cs
@@ -26,9 +26,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Tween
