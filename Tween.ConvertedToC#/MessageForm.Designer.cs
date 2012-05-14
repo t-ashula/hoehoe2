edbcab131bea75e85e97dad6c6997a9281f65e50
t.ashula <office@ashula.info>
2012-05-15 01:06:30 +0900

    MessageForm cleanuped

--- Tween.ConvertedToC#/MessageForm.Designer.cs
+++ Tween.ConvertedToC#/MessageForm.Designer.cs
@@ -1,4 +1,3 @@
-using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,63 +5,65 @@ using System.Drawing;
 using System.Windows.Forms;
 namespace Tween
 {
-	[Microsoft.VisualBasic.CompilerServices.DesignerGenerated()]
-	partial class MessageForm : System.Windows.Forms.Form
-	{
+    partial class MessageForm : System.Windows.Forms.Form
+    {
+        //フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+        [System.Diagnostics.DebuggerNonUserCode()]
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing && components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
 
-		//フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-		[System.Diagnostics.DebuggerNonUserCode()]
-		protected override void Dispose(bool disposing)
-		{
-			try {
-				if (disposing && components != null) {
-					components.Dispose();
-				}
-			} finally {
-				base.Dispose(disposing);
-			}
-		}
+        //Windows フォーム デザイナーで必要です。
+        private System.ComponentModel.IContainer components;
 
-		//Windows フォーム デザイナーで必要です。
+        //メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+        //Windows フォーム デザイナーを使用して変更できます。  
+        //コード エディターを使って変更しないでください。
 
-		private System.ComponentModel.IContainer components;
-		//メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
-		//Windows フォーム デザイナーを使用して変更できます。  
-		//コード エディターを使って変更しないでください。
-		[System.Diagnostics.DebuggerStepThrough()]
-		private void InitializeComponent()
-		{
-			this.Label1 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			//
-			//Label1
-			//
-			this.Label1.AutoSize = true;
-			this.Label1.Location = new System.Drawing.Point(123, 125);
-			this.Label1.Name = "Label1";
-			this.Label1.Size = new System.Drawing.Size(38, 12);
-			this.Label1.TabIndex = 0;
-			this.Label1.Text = "Label1";
-			//
-			//MessageForm
-			//
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6f, 12f);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoSize = true;
-			this.ClientSize = new System.Drawing.Size(284, 262);
-			this.Controls.Add(this.Label1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "MessageForm";
-			this.ShowIcon = false;
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "MessageForm";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            //
+            //Label1
+            //
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(123, 125);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(38, 12);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Label1";
+            //
+            //MessageForm
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6f, 12f);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.Label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MessageForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MessageForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-		}
-		internal System.Windows.Forms.Label Label1;
-	}
+        }
+        internal System.Windows.Forms.Label Label1;
+    }
 }
