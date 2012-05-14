f44d591373df66eb713fb46eb7d7ad4f4d1d44ed
t.ashula <office@ashula.info>
2012-05-15 06:54:30 +0900

    ListAvailable form cleanuped

--- Tween.ConvertedToC#/ListAvailable.cs
+++ Tween.ConvertedToC#/ListAvailable.cs
@@ -31,34 +31,31 @@ namespace Tween
 {
     public partial class ListAvailable
     {
-        private ListElement _selectedList = null;
+        public ListElement SelectedList { get; private set; }
 
-        public ListElement SelectedList
-        {
-            get { return _selectedList; }
-        }
-
-        private void OK_Button_Click(System.Object sender, System.EventArgs e)
+        private void OkButton_Click(object sender, EventArgs e)
         {
             if (this.ListsList.SelectedIndex > -1)
             {
-                _selectedList = (ListElement)this.ListsList.SelectedItem;
+                this.SelectedList = (ListElement)this.ListsList.SelectedItem;
                 this.DialogResult = System.Windows.Forms.DialogResult.OK;
                 this.Close();
             }
         }
 
-        private void Cancel_Button_Click(System.Object sender, System.EventArgs e)
+        private void CancelButton_Click(object sender, EventArgs e)
         {
-            _selectedList = null;
+            this.SelectedList = null;
             this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.Close();
         }
 
-        private void ListAvailable_Shown(object sender, System.EventArgs e)
+        private void ListAvailable_Shown(object sender, EventArgs e)
         {
             if (TabInformations.GetInstance().SubscribableLists.Count == 0)
+            {
                 this.RefreshLists();
+            }
             this.ListsList.Items.AddRange(TabInformations.GetInstance().SubscribableLists.ToArray());
             if (this.ListsList.Items.Count > 0)
             {
@@ -66,54 +63,43 @@ namespace Tween
             }
             else
             {
-                this.UsernameLabel.Text = "";
-                this.NameLabel.Text = "";
-                this.StatusLabel.Text = "";
-                this.MemberCountLabel.Text = "0";
-                this.SubscriberCountLabel.Text = "0";
-                this.DescriptionText.Text = "";
+                clearListInfo();
             }
         }
 
-        private void ListsList_SelectedIndexChanged(System.Object sender, System.EventArgs e)
+        private void ListsList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ListElement lst = null;
             if (this.ListsList.SelectedIndex > -1)
             {
-                lst = (ListElement)this.ListsList.SelectedItem;
+                setListInfo((ListElement)this.ListsList.SelectedItem);
             }
             else
             {
-                lst = null;
-            }
-            if (lst == null)
-            {
-                this.UsernameLabel.Text = "";
-                this.NameLabel.Text = "";
-                this.StatusLabel.Text = "";
-                this.MemberCountLabel.Text = "0";
-                this.SubscriberCountLabel.Text = "0";
-                this.DescriptionText.Text = "";
-            }
-            else
-            {
-                this.UsernameLabel.Text = lst.Username + " / " + lst.Nickname;
-                this.NameLabel.Text = lst.Name;
-                if (lst.IsPublic)
-                {
-                    this.StatusLabel.Text = "Public";
-                }
-                else
-                {
-                    this.StatusLabel.Text = "Private";
-                }
-                this.MemberCountLabel.Text = lst.MemberCount.ToString("#,##0");
-                this.SubscriberCountLabel.Text = lst.SubscriberCount.ToString("#,##0");
-                this.DescriptionText.Text = lst.Description;
+                clearListInfo();
             }
         }
 
-        private void RefreshButton_Click(System.Object sender, System.EventArgs e)
+        private void clearListInfo()
+        {
+            this.UsernameLabel.Text = "";
+            this.NameLabel.Text = "";
+            this.StatusLabel.Text = "";
+            this.MemberCountLabel.Text = "0";
+            this.SubscriberCountLabel.Text = "0";
+            this.DescriptionText.Text = "";
+        }
+
+        private void setListInfo(ListElement lst)
+        {
+            this.UsernameLabel.Text = String.Format("{0} / {1}", lst.Username, lst.Nickname);
+            this.NameLabel.Text = lst.Name;
+            this.StatusLabel.Text = lst.IsPublic ? "Public" : "Private";
+            this.MemberCountLabel.Text = lst.MemberCount.ToString("#,##0");
+            this.SubscriberCountLabel.Text = lst.SubscriberCount.ToString("#,##0");
+            this.DescriptionText.Text = lst.Description;
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
         {
             this.RefreshLists();
             this.ListsList.Items.Clear();
@@ -129,7 +115,7 @@ namespace Tween
             using (FormInfo dlg = new FormInfo(this, "Getting Lists...", RefreshLists_DoWork))
             {
                 dlg.ShowDialog();
-                if (!string.IsNullOrEmpty((string)dlg.Result))
+                if (!String.IsNullOrEmpty((string)dlg.Result))
                 {
                     MessageBox.Show("Failed to get lists. (" + (string)dlg.Result + ")");
                     return;
@@ -143,7 +129,7 @@ namespace Tween
             {
                 e.Result = ((TweenMain)this.Owner).TwitterInstance.GetListsApi();
             }
-            catch (InvalidCastException ex)
+            catch (InvalidCastException)
             {
                 return;
             }
@@ -151,7 +137,7 @@ namespace Tween
 
         public ListAvailable()
         {
-            Shown += ListAvailable_Shown;
+            SelectedList = null;
             InitializeComponent();
         }
     }
