e7f4f56bc412607719ea81a6e1ec1f58e30e7a98
t.ashula <dev@ashula.info>
2012-05-16 15:17:47 +0900

    IMultimediaShareService cleanuped

--- Tween.ConvertedToC#/Connection/IMultimediaShareService.cs
+++ Tween.ConvertedToC#/Connection/IMultimediaShareService.cs
@@ -27,18 +27,12 @@ namespace Tween
 {
     public interface IMultimediaShareService
     {
-        string Upload(ref string filePath, ref string message, long reply_to);
-
+        string Upload(ref string filePath, ref string message, long replyTo);
         bool CheckValidExtension(string ext);
-
         string GetFileOpenDialogFilter();
-
         Tween.MyCommon.UploadFileType GetFileType(string ext);
-
         bool IsSupportedFileType(Tween.MyCommon.UploadFileType type);
-
         bool CheckValidFilesize(string ext, long fileSize);
-
         bool Configuration(string key, object value);
     }
 }
