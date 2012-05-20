ba516c4a9f1210405549a90ce227f35d76eb7ad3
t.ashula <office@ashula.info>
2012-05-20 10:14:51 +0900

    remove IsVaidEmail from MyCommon

--- Tween.ConvertedToC#/MyCommon.cs
+++ Tween.ConvertedToC#/MyCommon.cs
@@ -36,7 +36,6 @@ using System.Reflection;
 using System.Runtime.Serialization.Json;
 using System.Security.Principal;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Web;
 using System.Windows.Forms;
 
@@ -434,12 +433,6 @@ namespace Hoehoe
             {
                 return false;
             }
-        }
-
-        public static bool IsValidEmail(string strIn)
-        {
-            // Return true if strIn is in valid e-mail format.
-            return Regex.IsMatch(strIn, "^(?(\")(\".+?\"@)|(([0-9a-zA-Z]((\\.(?!\\.))|[-!#\\$%&'\\*\\+/=\\?\\^`\\{\\}\\|~\\w])*)(?<=[0-9a-zA-Z])@))" + "(?(\\[)(\\[(\\d{1,3}\\.){3}\\d{1,3}\\])|(([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,6}))$");
         }
 
         public static string GetAppDir()
