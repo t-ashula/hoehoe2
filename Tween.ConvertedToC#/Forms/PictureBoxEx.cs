076cbc311036e5d3cc032e2aae222a5a5fb9a09e
t.ashula <dev@ashula.info>
2012-05-21 23:43:29 +0900

    cleanup PictureBoxEx using StyleCop

--- Tween.ConvertedToC#/Forms/PictureBoxEx.cs
+++ Tween.ConvertedToC#/Forms/PictureBoxEx.cs
@@ -24,11 +24,11 @@
 // the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
 // Boston, MA 02110-1301, USA.
 
-using System;
-using System.Windows.Forms;
-
 namespace Hoehoe.TweenCustomControl
 {
+    using System;
+    using System.Windows.Forms;
+
     public class PictureBoxEx : PictureBox
     {
         protected override void OnPaint(PaintEventArgs pe)
