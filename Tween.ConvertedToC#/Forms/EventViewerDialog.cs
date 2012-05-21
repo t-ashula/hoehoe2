a17df1cbad19ba8e69a56ed9268a74b386f78709
t.ashula <dev@ashula.info>
2012-05-21 20:46:32 +0900

    cleanup EventViewDialog form using StyleCop

--- Tween.ConvertedToC#/Forms/EventViewerDialog.cs
+++ Tween.ConvertedToC#/Forms/EventViewerDialog.cs
@@ -24,25 +24,37 @@
 // the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
 // Boston, MA 02110-1301, USA.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Windows.Forms;
-
 namespace Hoehoe
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Windows.Forms;
+
     public partial class EventViewerDialog
     {
-        public List<Twitter.FormattedEvent> EventSource { get; set; }
+        #region privates
+        private Twitter.FormattedEvent[] filterdEventSource;
+        private ListViewItem[] itemCache;
+        private int itemCacheIndex;
+        private TabPage curTab;
+        #endregion
 
-        private Twitter.FormattedEvent[] _filterdEventSource;
-        private ListViewItem[] _itemCache;
-        private int _itemCacheIndex;
-        private TabPage _curTab;
+        #region constructor
+        public EventViewerDialog()
+        {
+            InitializeComponent();
+        }
+        #endregion
 
+        #region properties
+        public List<Hoehoe.Twitter.FormattedEvent> EventSource { get; set; }
+        #endregion
+
+        #region event handler
         private void OK_Button_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.OK;
@@ -55,91 +67,45 @@ namespace Hoehoe
             this.Close();
         }
 
-        private ListViewItem CreateListViewItem(Twitter.FormattedEvent source)
-        {
-            return new ListViewItem(new[] { source.CreatedAt.ToString(), source.Event.ToUpper(), source.Username, source.Target });
-        }
-
         private void EventViewerDialog_Shown(object sender, EventArgs e)
         {
             EventList.BeginUpdate();
-            _curTab = TabEventType.SelectedTab;
-            CreateFilterdEventSource();
+            this.curTab = TabEventType.SelectedTab;
+            this.CreateFilterdEventSource();
             EventList.EndUpdate();
             this.TopMost = AppendSettingDialog.Instance.AlwaysTop;
         }
 
         private void EventList_DoubleClick(object sender, EventArgs e)
         {
-            if (!(EventList.SelectedIndices.Count == 0) && _filterdEventSource[EventList.SelectedIndices[0]] != null)
+            if (EventList.SelectedIndices.Count != 0)
             {
-                ((TweenMain)this.Owner).OpenUriAsync("http://twitter.com/" + _filterdEventSource[EventList.SelectedIndices[0]].Username);
-            }
-        }
-
-        private EventType ParseEventTypeFromTag()
-        {
-            return (EventType)Enum.Parse(typeof(EventType), _curTab.Tag.ToString());
-        }
-
-        private bool IsFilterMatch(Twitter.FormattedEvent x)
-        {
-            if (!CheckBoxFilter.Checked || String.IsNullOrEmpty(TextBoxKeyword.Text))
-            {
-                return true;
-            }
-            if (CheckRegex.Checked)
-            {
-                try
+                var selectedEvent = this.filterdEventSource[EventList.SelectedIndices[0]];
+                if (selectedEvent != null)
                 {
-                    Regex rx = new Regex(TextBoxKeyword.Text);
-                    return rx.Match(x.Username).Success || rx.Match(x.Target).Success;
+                    ((TweenMain)this.Owner).OpenUriAsync("http://twitter.com/" + selectedEvent.Username);
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(Hoehoe.Properties.Resources.ButtonOK_ClickText3 + ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                    return false;
-                }
-            }
-            else
-            {
-                return x.Username.Contains(TextBoxKeyword.Text) || x.Target.Contains(TextBoxKeyword.Text);
-            }
-        }
-
-        private void CreateFilterdEventSource()
-        {
-            if (EventSource != null && EventSource.Count > 0)
-            {
-                _filterdEventSource = EventSource.FindAll(x => CheckExcludeMyEvent.Checked ? !x.IsMe : true && Convert.ToBoolean(x.Eventtype & ParseEventTypeFromTag()) && IsFilterMatch(x)).ToArray();
-                _itemCache = null;
-                EventList.VirtualListSize = _filterdEventSource.Count();
-                StatusLabelCount.Text = String.Format("{0} / {1}", _filterdEventSource.Count(), EventSource.Count);
-            }
-            else
-            {
-                StatusLabelCount.Text = "0 / 0";
             }
         }
 
         private void CheckExcludeMyEvent_CheckedChanged(object sender, EventArgs e)
         {
-            CreateFilterdEventSource();
+            this.CreateFilterdEventSource();
         }
 
         private void ButtonRefresh_Click(object sender, EventArgs e)
         {
-            CreateFilterdEventSource();
+            this.CreateFilterdEventSource();
         }
 
         private void TabEventType_SelectedIndexChanged(object sender, EventArgs e)
         {
-            CreateFilterdEventSource();
+            this.CreateFilterdEventSource();
         }
 
         private void TabEventType_Selecting(object sender, TabControlCancelEventArgs e)
         {
-            _curTab = e.TabPage;
+            this.curTab = e.TabPage;
             if (!e.TabPage.Controls.Contains(EventList))
             {
                 e.TabPage.Controls.Add(EventList);
@@ -150,66 +116,46 @@ namespace Hoehoe
         {
             if (e.KeyChar == (char)Keys.Enter)
             {
-                CreateFilterdEventSource();
+                this.CreateFilterdEventSource();
                 e.Handled = true;
             }
         }
 
         private void EventList_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
         {
-            if (_itemCache != null && e.ItemIndex >= _itemCacheIndex && e.ItemIndex < _itemCacheIndex + _itemCache.Length)
+            if (this.itemCache != null && e.ItemIndex >= this.itemCacheIndex && e.ItemIndex < this.itemCacheIndex + this.itemCache.Length)
             {
-                //キャッシュヒット
-                e.Item = _itemCache[e.ItemIndex - _itemCacheIndex];
+                // キャッシュヒット
+                e.Item = this.itemCache[e.ItemIndex - this.itemCacheIndex];
             }
             else
             {
-                //キャッシュミス
-                e.Item = CreateListViewItem(_filterdEventSource[e.ItemIndex]);
+                // キャッシュミス
+                e.Item = this.CreateListViewItem(this.filterdEventSource[e.ItemIndex]);
             }
         }
 
         private void EventList_CacheVirtualItems(object sender, CacheVirtualItemsEventArgs e)
         {
-            CreateCache(e.StartIndex, e.EndIndex);
+            this.CreateCache(e.StartIndex, e.EndIndex);
         }
-
-        private void CreateCache(int startIndex, int endIndex)
-        {
-            //キャッシュ要求（要求範囲±30を作成）
-            startIndex -= 30;
-            if (startIndex < 0)
-            {
-                startIndex = 0;
-            }
-            endIndex += 30;
-            if (endIndex > _filterdEventSource.Count() - 1)
-            {
-                endIndex = _filterdEventSource.Count() - 1;
-            }
-            _itemCache = new ListViewItem[endIndex - startIndex + 1];
-            _itemCacheIndex = startIndex;
-            for (int i = 0; i <= endIndex - startIndex; i++)
-            {
-                _itemCache[i] = CreateListViewItem(_filterdEventSource[startIndex + i]);
-            }
-        }
-
+        
         private void SaveLogButton_Click(object sender, EventArgs e)
         {
             DialogResult rslt = MessageBox.Show(string.Format(Hoehoe.Properties.Resources.SaveLogMenuItem_ClickText5, Environment.NewLine), Hoehoe.Properties.Resources.SaveLogMenuItem_ClickText2, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
-            string tabName = "";
+            string tabName = string.Empty;
             switch (rslt)
             {
                 case DialogResult.Yes:
-                    tabName = _curTab.Tag.ToString();
+                    tabName = (string)this.curTab.Tag;
                     break;
                 case DialogResult.No:
                     break;
                 default:
                     return;
             }
-            SaveFileDialog1.FileName = String.Format("HoehoeEvents{0}{1:yyMMdd-HHmmss}.tsv", tabName, DateTime.Now);
+
+            SaveFileDialog1.FileName = string.Format("HoehoeEvents{0}{1:yyMMdd-HHmmss}.tsv", tabName, DateTime.Now);
             SaveFileDialog1.InitialDirectory = MyCommon.AppDir;
             SaveFileDialog1.Filter = Hoehoe.Properties.Resources.SaveLogMenuItem_ClickText3;
             SaveFileDialog1.FilterIndex = 0;
@@ -222,37 +168,111 @@ namespace Hoehoe
                 {
                     return;
                 }
+
                 using (StreamWriter sw = new StreamWriter(SaveFileDialog1.FileName, false, Encoding.UTF8))
                 {
                     switch (rslt)
                     {
                         case DialogResult.Yes:
-                            SaveEventLog(_filterdEventSource.ToList(), sw);
+                            this.SaveEventLog(this.filterdEventSource.ToList(), sw);
                             break;
                         case DialogResult.No:
-                            SaveEventLog(EventSource, sw);
+                            this.SaveEventLog(this.EventSource, sw);
                             break;
                         default:
                             break;
                     }
-                    sw.Close();
-                    sw.Dispose();
                 }
             }
+
             this.TopMost = AppendSettingDialog.Instance.AlwaysTop;
+        }
+        #endregion
+
+        #region private methods
+        private ListViewItem CreateListViewItem(Twitter.FormattedEvent source)
+        {
+            return new ListViewItem(new[] { source.CreatedAt.ToString(), source.Event.ToUpper(), source.Username, source.Target });
+        }
+
+        private EventType ParseEventTypeFromTag()
+        {
+            return (EventType)Enum.Parse(typeof(EventType), (string)this.curTab.Tag);
+        }
+
+        private bool IsFilterMatch(Twitter.FormattedEvent x)
+        {
+            if (!CheckBoxFilter.Checked || string.IsNullOrEmpty(TextBoxKeyword.Text))
+            {
+                return true;
+            }
+
+            if (CheckRegex.Checked)
+            {
+                try
+                {
+                    Regex rx = new Regex(TextBoxKeyword.Text);
+                    return rx.Match(x.Username).Success || rx.Match(x.Target).Success;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(Hoehoe.Properties.Resources.ButtonOK_ClickText3 + ex.Message, string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+            }
+            else
+            {
+                return x.Username.Contains(TextBoxKeyword.Text) || x.Target.Contains(TextBoxKeyword.Text);
+            }
+        }
+
+        private void CreateFilterdEventSource()
+        {
+            if (this.EventSource != null && this.EventSource.Count > 0)
+            {
+                this.filterdEventSource = this.EventSource.FindAll(x => CheckExcludeMyEvent.Checked ? !x.IsMe : true
+                    && Convert.ToBoolean(x.Eventtype & this.ParseEventTypeFromTag())
+                    && this.IsFilterMatch(x)).ToArray();
+                this.itemCache = null;
+                this.EventList.VirtualListSize = this.filterdEventSource.Count();
+                this.StatusLabelCount.Text = string.Format("{0} / {1}", this.filterdEventSource.Count(), this.EventSource.Count);
+            }
+            else
+            {
+                this.StatusLabelCount.Text = "0 / 0";
+            }
+        }
+
+        private void CreateCache(int startIndex, int endIndex)
+        {
+            // キャッシュ要求（要求範囲±30を作成）
+            startIndex -= 30;
+            if (startIndex < 0)
+            {
+                startIndex = 0;
+            }
+
+            endIndex += 30;
+            if (endIndex > this.filterdEventSource.Count() - 1)
+            {
+                endIndex = this.filterdEventSource.Count() - 1;
+            }
+
+            this.itemCache = new ListViewItem[endIndex - startIndex + 1];
+            this.itemCacheIndex = startIndex;
+            for (int i = 0; i <= endIndex - startIndex; i++)
+            {
+                this.itemCache[i] = this.CreateListViewItem(this.filterdEventSource[startIndex + i]);
+            }
         }
 
         private void SaveEventLog(List<Twitter.FormattedEvent> source, StreamWriter sw)
         {
-            foreach (Twitter.FormattedEvent ev in source)
+            foreach (var ev in source)
             {
-                sw.WriteLine(String.Format("{0}\t\"{1}\"\t{2}\t{3}\t{4}\t{5}", ev.Eventtype, ev.CreatedAt, ev.Event, ev.Username, ev.Target, ev.Id));
+                sw.WriteLine(string.Format("{0}\t\"{1}\"\t{2}\t{3}\t{4}\t{5}", ev.Eventtype, ev.CreatedAt, ev.Event, ev.Username, ev.Target, ev.Id));
             }
         }
-
-        public EventViewerDialog()
-        {
-            InitializeComponent();
-        }
+        #endregion
     }
 }
