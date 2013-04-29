1bbc9699f8748dff587cdc42386c20b37b18b6ed
t-ashula <t.ashula+github@gmail.com>
2013-04-29 19:03:46 +0900

    remove unhandle exception handler

--- Hh2Up.ConvertedToC#/Program.cs
+++ Hh2Up.ConvertedToC#/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace TweenUp
@@ -11,16 +10,10 @@ namespace TweenUp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(true);
-            Application.ThreadException += MyApplication_UnhandledException;
-            Application.ApplicationExit += (_, __) => { };
-            Application.Run(new Form1());
-        }
-
-        private static void MyApplication_UnhandledException(object sender, ThreadExceptionEventArgs e)
-        {
-            MessageBox.Show(
+            Application.ThreadException += (sender, e) => MessageBox.Show(
                 My.Resources.ExceptionMessage + Environment.NewLine + e.Exception.Message,
                 My.Resources.ExceptionTitle);
+            Application.Run(new Form1());
         }
     }
 }
