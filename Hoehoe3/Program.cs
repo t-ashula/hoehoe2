28fa82cbbbaba50b06e770e78000efed1873e094
t-ashula <t.ashula+github@gmail.com>
2015-03-05 20:04:07 +0900

    add hoehoe.next project stub

---
+++ Hoehoe3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Hoehoe3
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
